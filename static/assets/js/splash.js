7282ff65fc6e92808fca496744203cb9062bc7e2
AIMMXI <aimmxi@aimmxi.xyz>
2026-04-10 00:55:09 +0200

    Updated index to not render the disclaimer for previews without Javascript

--- static/assets/js/splash.js
+++ static/assets/js/splash.js
@@ -1,28 +1,30 @@
 // The elements are fetched
-const splashc = document.getElementById('splash-c');
-const splashbutton = document.getElementById('splash-button');
+const splashContainer = document.getElementById('splash-c');
+const splashButton = document.getElementById('splash-button');
 const body = document.getElementById('body');
 
 // The URL is parsed to check for the existance of the noanim flag
 const urlParams = new URLSearchParams(window.location.search);
 
-
-// If the URL does not contain noanim, the splash screen is displayed
+// If the URL does not contain noanim
 if (!urlParams.has('noanim')) {
+	// Initially, the splash is hidden at all times and only after validating the URL is it presented to the user
+	splashContainer.classList.remove('fade');
+
 	// Upon clicking the enter box
-	splashbutton.addEventListener('click', function() {
+	splashButton.addEventListener('click', function() {
 		// The splash-c is faded
-		splashc.classList.add('fade');
+		splashContainer.classList.add('fade');
 
 		// After 900ms, to avoid scrolling all splashes are hidden an the overflow-y is set to behave as default
 		setTimeout(() => {
-			splashc.style.display = 'none';
+			splashContainer.style.display = 'none';
 			body.style.overflowY = 'visible';
 		}, 900);
 	});
 } else {
-	// If the flag is present, all the splashes are hidden and the overflow is configured as default
-	splashc.style.display = 'none';
+	// If the flag is present, the splash does not get displayed at all and the overflow is set to normal
+	splashContainer.style.display = 'none';
 	body.style.overflowY = 'visible';
 }
 
