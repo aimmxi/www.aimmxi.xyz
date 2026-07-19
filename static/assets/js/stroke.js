3d83196b8dab4b3a380a95b39504203130da42ec
AIMMXI <aimmxi@aimmxi.xyz>
2026-07-19 22:16:21 +0200

    Added new bio pictures

--- static/assets/js/stroke.js
+++ static/assets/js/stroke.js
@@ -17,7 +17,6 @@ function drawStroke(options) {
   const ctx = canvas.getContext("2d");
   const width = canvas.width;
   const height = canvas.height;
-  console.log(width, height)
 
   // Variables
   const images = [];        // Array of images for the stroke
